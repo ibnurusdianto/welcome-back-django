506ef180bb8f507c4a00d464b17fffe8135d742b
buble-security <sensegamer4@gmail.com>
2026-03-05 08:57:14 +0700

    updated day

--- employess/views.py
+++ employess/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from employess.models import employee
+from django.http import Http404
 
-# Create your views here.
+def employee_detail(request, pk):
+    emp = get_object_or_404(employee, pk=pk)
+    context = {'employee': emp}
+    return render(request, 'employee_detail.html', context)
+
